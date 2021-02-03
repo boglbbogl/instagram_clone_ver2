cca00d0eb5efd8760586e76e3d2483b7d89fa8d3
Tyger Hwang <boglbbogl@gmail.com>
2021-02-03 22:49:38 +0900

    BottomNavigationBar - ver.1

---
+++ lib/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<BottomNavigationBarItem> btmNavItems = [
+    BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+    BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
+    BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
+    BottomNavigationBarItem(icon: Icon(Icons.healing), label: ''),
+    BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: ''),
+  ];
+
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.amberAccent,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          items: btmNavItems,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.black87,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _selectedIndex,
+        onTap: _onTabItem,
+      ),
+    );
+  }
+
+  void _onTabItem(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
