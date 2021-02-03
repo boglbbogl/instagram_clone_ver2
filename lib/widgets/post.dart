e8acc3cf2b728578c1dea73657e10e12179d3bbe
Tyger Hwang <boglbbogl@gmail.com>
2021-02-04 01:13:42 +0900

    feed_screen - post_widget _postHeader

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone_ver2/constant/common_size.dart';
 
 import 'my_progress_indicator.dart';
 
@@ -8,30 +9,63 @@ class Post extends StatelessWidget {
 
   Size size;
 
-  Post(this.index, {
+  Post(
+    this.index, {
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if(size==null)
-      size = MediaQuery.of(context).size;
+    if (size == null) size = MediaQuery.of(context).size;
 
+    return Column(
+      children: [
+        _postHeader(),
+        _postImage(),
+      ],
+    );
+  }
+
+  Widget _postHeader() {
+    return Row(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(common_xxs_gap),
+          child: ClipOval(
+            child: CachedNetworkImage(
+              imageUrl: 'https://picsum.photos/100',
+              width: avatar_size,
+              height: avatar_size,
+            ),
+          ),
+        ),
+        Expanded(child: Text('username')),
+        IconButton(
+          icon: Icon(Icons.more_horiz),
+          onPressed: null,
+          color: Colors.black87,
+        ),
+      ],
+    );
+  }
+
+  CachedNetworkImage _postImage() {
     return CachedNetworkImage(
-      imageUrl: 'https://picsum.photos/id/$index/3000/3000',
-      placeholder: (BuildContext context, String url){
-        return MyProgressIndicator(containerSize: size.width,);
+      imageUrl: 'https://picsum.photos/id/$index/200/200',
+      placeholder: (BuildContext context, String url) {
+        return MyProgressIndicator(
+          containerSize: size.width,
+        );
       },
-      imageBuilder: (BuildContext context, ImageProvider imageProvider){
+      imageBuilder: (BuildContext context, ImageProvider imageProvider) {
         return AspectRatio(
           aspectRatio: 1,
           child: Container(
             decoration: BoxDecoration(
-              image: DecorationImage(
-                image: imageProvider,
-                fit: BoxFit.cover,
-              )
-            ),
+                image: DecorationImage(
+              image: imageProvider,
+              fit: BoxFit.cover,
+            )),
           ),
         );
       },
