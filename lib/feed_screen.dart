99b7f6fce805972f916acdeeab9b9ab33e184070
Tyger Hwang <boglbbogl@gmail.com>
2021-02-04 00:36:10 +0900

    feed_screen - post_widget ver - 1

--- lib/feed_screen.dart
+++ lib/feed_screen.dart
@@ -1,16 +1,44 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'widgets/post.dart';
 
 class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Instagram',
-          style: TextStyle(fontFamily: 'VeganStyle', color: Colors.black87),
-        ),
+      appBar: CupertinoNavigationBar(
+          leading: IconButton(
+            onPressed: null,
+            icon: Icon(Icons.camera_alt,
+              color: Colors.red,
+            ),
+          ),
+          middle: Text(
+            'Instagram',
+            style: TextStyle(fontFamily: 'VeganStyle', color: Colors.black87),
+          ),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              IconButton(
+              onPressed: null,
+              icon: ImageIcon(
+                  AssetImage('assets/images/actionbar_camera.png'),
+                color: Colors.deepPurple,
+
+              ),
+              ),
+            ],
+          )
       ),
+      body: ListView.builder(itemBuilder: feedListBuilder, itemCount: 3,),
     );
   }
+
+  Widget feedListBuilder(BuildContext context, int index) {
+    return Post(index);
+  }
 }
+
+
